0f3e9bf0a8baccfeeb019966dbe4f38e03496ae3
pellegoeg <pellegoeg@users.noreply.github.com>
2021-12-13 09:53:00 +0100

    Updated windows and README

--- mac_install_simple.sh
+++ mac_install_simple.sh
@@ -1,10 +1,8 @@
 #!/bin/zsh
 
-# Config
-root=/Users/$USER
-col_norm='\033[0;32m'
-col_impo='\033[0;31m'
-col_end='\033[0m'
+
+# Import functions to script
+. ./shell_functions.sh
 
 # This file installs and configures 3 parts
 #     Miniconda and the base python installation, setting up the build conda environment
@@ -13,19 +11,19 @@ col_end='\033[0m'
 
 # Introduction to what is going to happen
 cd $root
-echo $col_norm Denne fil vil installere python til brug i kurset $col_end
-echo .
-echo $col_norm Dette program har tre forskellige farver $col_end
-echo $col_norm Denne farve er hjaelpe tekst, som guider dig igennem installationen $col_end
+p_n "Denne fil vil installere python til brug i kurset"
+p_s
+p_n "Dette program har tre forskellige farver"
+p_n "Denne farve er hjaelpe tekst, som guider dig igennem installationen"
 echo Denne farve er tekst skrevet af programmerne der installeres
-echo $col_impo Denne farve er naar der er noget vigtigt du skal vaere opmaerksom paa $col_end
-echo .
-echo $col_impo FOER DU GAAR IGANG $col_end
-echo $col_impo Hav mindst \1 GB plads ledig. Der gives hjaelp til afinstallation i slutningen af kurset $col_end
-echo $col_impo Deaktiver evt. antivirus du har installeret, mens installationen foregaar $col_end
-echo $col_impo Efter du trykker enter skal du give muligvis give dit password du bruger til denne computer $col_end
-echo .
-echo $col_norm Naar du trykker enter, vil miniconda blive downloadet og installeret $col_end
+p_i "Denne farve er naar der er noget vigtigt du skal vaere opmaerksom paa"
+p_s
+p_i "FOER DU GAAR IGANG"
+p_i "Hav mindst \1 GB plads ledig. Der gives hjaelp til afinstallation i slutningen af kurset"
+p_i "Deaktiver evt. antivirus du har installeret, mens installationen foregaar"
+p_i "Efter du trykker enter skal du give muligvis give dit password du bruger til denne computer"
+p_s
+p_n "Naar du trykker enter, vil miniconda blive downloadet og installeret"
 read something
 
 # Download homebrew (only for mac)
@@ -35,11 +33,11 @@ read something
 brew install --cask miniconda
 conda init "$(basename "${SHELL}")"
 
-echo $col_norm Miniconda er er et program som holder styr paa programmeringssproget python $col_end
-echo .
-echo $col_norm Alle programmer til dette kursus burde nu vaere korrekt installeret og opsat$col_end
-echo $col_norm Maaden du tester det paa, er at aabne en terminal og skrive to kommandoer $col_end
-echo $col_norm python --version $col_end
-echo $col_norm Dette viser hvilken python version der er installeret i miniconda $col_end
-echo $col_norm Naar du trykker skulle terminalen gerne lukke, ellers tryk paa X et par gange oeverst til hoejre$col_end
+p_n "Miniconda er er et program som holder styr paa programmeringssproget python"
+p_s
+p_n "Alle programmer til dette kursus burde nu være korrekt installeret og opsat"
+p_n "Måden du tester det på, er at aabne en terminal og skrive to kommandoer"
+p_n "python --version"
+p_n "Dette viser hvilken python version der er installeret i miniconda"
+p_n "Naar du trykker skulle terminalen gerne lukke, ellers tryk paa X et par gange oeverst til højre"
 read something
