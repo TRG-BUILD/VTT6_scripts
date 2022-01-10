5fb6eefeb7e56f0f79dfa47c4ffa7451398c6ccb
pellegoeg <pellegoeg@users.noreply.github.com>
2022-01-10 12:36:04 +0100

    adding command line script to easy start postgres

---
+++ start_postgres.cmd
@@ -0,0 +1,14 @@
+@ECHO off
+chcp 1252 >nul
+
+call %root%\miniconda3\Scripts\activate.bat %root%\miniconda3  >> %logfile%
+
+Rem Create Conda environment
+call conda activate build
+
+echo "Starting postgresql database..."
+pg_ctl -D %USERPROFILE%/pgdatabase -l logfile start
+
+echo "Pres any key to stop postgresql database..."
+pause
+pg_ctl -D %USERPROFILE%/pgdatabase -l logfile stop
